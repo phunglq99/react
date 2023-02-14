c459280a63227fee6c2abdf13c352b22a9d152b0
Andy <vu.ngoc@news.com.au>
2023-02-14 15:35:09 +0700

    14/2

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -8,27 +8,40 @@ import { useEffect, useState } from "react"
 2. useEffect(callback,[])
     + Chỉ gọi callback 1 lần sau khi component mounted
 3. useEffect(callback,[deps])
-    +
+    + Callback sẽ được gọi lại mỗi khi dependecies thay đổi
 */
 
 // ------------------ý chung của 3 thành phần trên
 // 1. Callback luôn được gọi sau khi Component mounted
 
+const tabs = ['posts', 'comments','albums', 'photos', 'todos', 'users']
+
 function Content() {
     const [posts, setPosts] = useState([])
-
     const [title, setTitle] = useState('')
+    const [type, setType] = useState('posts')
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
             .then(res => res.json())
             .then(posts => {
                 setPosts(posts);
             })
-    },[])
+    },[type])
 
     return (
         <div>
+            {tabs.map(tab => {
+                return (
+                    <button key={tab}
+                        style={type === tab ? {color: 'white', backgroundColor: 'red'} : {}}
+                        onClick ={() => setType(tab)}
+                    >
+                        {tab}
+                    </button>
+                )
+            })}
+
             <input
                 type="text"
                 value={title}
@@ -37,7 +50,7 @@ function Content() {
             <ul>
                 {posts.map(post => {
                     return (
-                        <li key={post.id}>{post.title}</li>
+                        <li key={post.id}>{post.title || post.name}</li>
                     )
                 })}
             </ul>
